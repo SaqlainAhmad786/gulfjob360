1692782b58f94b496c45a8862a50b3f4ca65e42b
Saqlain Ahmad <123837216+SaqlainAhmad786@users.noreply.github.com>
2024-12-03 17:40:43 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 function handleSideBar() {
     document.getElementById("my-drawer-2").checked = false
 }
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    const bottomNavbar = document.querySelector(".bottomNavbar");
+    const scrollY = window.scrollY;
+    const bottomReached = window.innerHeight + scrollY >= document.body.offsetHeight;
+
+    if (bottomReached) {
+        // Hide navbar when at the bottom
+        bottomNavbar.classList.add("translate-y-[100%]");
+    } else {
+        // Show navbar when scrolling up
+        bottomNavbar.classList.remove("translate-y-[100%]");
+    }
+
+    lastScrollY = scrollY;
+});
